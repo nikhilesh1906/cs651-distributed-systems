bf6498c140cd7efe4b22458c2c4a3e86ebc7c0fc
nikhilesh1906 <nikm@bu.edu>
2019-09-10 11:39:00 -0400

    doMap doReduce implemented

--- src/mapreduce/common_map.go
+++ src/mapreduce/common_map.go
@@ -1,7 +1,11 @@
 package mapreduce
 
 import (
+	"encoding/json"
 	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 // doMap does the job of a map worker: it reads one of the input files
@@ -29,7 +33,7 @@ func doMap(
 	//
 	// One format often used for serializing data to a byte stream that the
 	// other end can correctly reconstruct is JSON. You are not required to
-	// use JSON, but as the output of the reduce tasks *must* be JSON,
+	// use JSON, but as the output of the reduce tasks must be JSON,
 	// familiarizing yourself with it here may prove useful. You can write
 	// out a data structure as a JSON string to a file using the commented
 	// code below. The corresponding decoding functions can be found in
@@ -40,10 +44,50 @@ func doMap(
 	//     err := enc.Encode(&kv)
 	//
 	// Remember to close the file after you have written all the values!
+
+	data, err := ioutil.ReadFile(inFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 利用 mapF 函数处理 inFile 文件内容，得到 kvs
+	kvs := mapF(inFile, string(data))
+
+	// 把 kvs 按照 R 任务的需求，划分成 nReduce 份
+	for i := 0; i < nReduce; i++ {
+		// Reduce 任务 i ，创建文件
+		filename := reduceName(jobName, mapTaskNumber, i)
+		file, err := os.Create(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// 把 file 作为 json 流式编码器的输出目的地
+		enc := json.NewEncoder(file)
+
+		for _, kv := range kvs {
+			if ihash(kv.Key)%nReduce != i {
+				// 根据题意，
+				// 删除掉不需要 Reduce 任务 i 处理的 kv
+				continue
+			}
+
+			// 对 kv 进行编码后，发送到 file
+			err := enc.Encode(&kv)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		// Map 任务 m 为 Reduce 任务 i 准备处理的内容
+		// 已经全部收集完毕，所以，可以关闭 file 了
+		file.Close()
+
+	}
 }
 
-func ihash(s string) uint32 {
+func ihash(s string) int {
 	h := fnv.New32a()
 	h.Write([]byte(s))
-	return h.Sum32()
+	return int(h.Sum32() & 0x7fffffff)
 }
